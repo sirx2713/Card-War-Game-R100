b91094c5889f2a9553f55b1f683089a4ef832f10
Tafadzwa Alexander Razaro <bradx2713@gmail.com>
2024-11-10 18:15:15 +0200

    Back to Swift!

--- Card War Game R1/ContentView.swift
+++ Card War Game R1/ContentView.swift
@@ -8,14 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var playerScore = 0
+    @State var computerScore = 0
+    @State var playerCard = "back"
+    @State var computerCard = "back"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Image("background-plain")
+                .ignoresSafeArea()
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(computerCard)
+                    Spacer()
+                }
+                Spacer()
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .padding(.all)
+                        Text("\(playerScore)")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .padding(.all)
+                        Text("\(computerScore)")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
         }
-        .padding()
+    }
+    
+    func deal(){
+        
+        //Randomise the player cards
+        let playerCardValue = Int.random(in: 2...14)
+        playerCard = "card" + String(playerCardValue)
+        
+        //Randomise the computer cards
+        let computerCardValue = Int.random(in: 2...14)
+        computerCard = "card" + String(computerCardValue)
+        
+        //Increase the scores
+        if playerCard > computerCard{
+            playerScore += 5
+        }
+        else if playerCard < computerCard{
+            computerScore += 5
+        }
+        else if playerCard == computerCard{
+            playerScore += 1
+            computerScore += 1
+        }
     }
 }
 
